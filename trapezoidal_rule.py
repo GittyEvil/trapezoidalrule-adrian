64d222033905478823c4fe61581cd6483267986e
Adrian S <89584348+GittyEvil@users.noreply.github.com>
2022-04-12 15:12:46 +0200

    Add files via upload

---
+++ trapezoidal_rule.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#våran f(x)=y och a,b,n värden
+f = lambda x : 1/(1 + x**2)
+a = 0; b = 5; N = 10
+
+# x and y values for the trapezoid rule
+x = np.linspace(a,b,N+1)
+y = f(x)
+
+# X and Y values for plotting y=f(x)
+X = np.linspace(a,b,100)
+Y = f(X)
+plt.plot(X,Y)
+#själva trapetsregel formeln
+for i in range(N):
+    xs = [x[i],x[i],x[i+1],x[i+1]]
+    ys = [0,f(x[i]),f(x[i+1]),0]
+    plt.fill(xs,ys,'b',edgecolor='b',alpha=0.2)
+#titel och show för att visa grafen
+plt.title('Trapezoid Rule, N = {}'.format(N))
+plt.show()
